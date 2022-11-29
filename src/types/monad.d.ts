385cfd00949eb7c8f77f0a63a0c4ce13f95c0c85
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-11-29 13:50:19 +0100

    Created Monad types.

---
+++ src/types/monad.d.ts
@@ -0,0 +1,24 @@
+export type Maybe<T> = T | undefined | null;
+
+export const maybe_do = <T, RT>(
+    optional: Maybe<T>,
+    callback: (val: T) => RT
+) => {
+    if (optional === null || optional === undefined) {
+        return null;
+    }
+
+    return callback(optional as T);
+};
+
+export const maybe_or = <T, RT>(
+    optional: Maybe<T>,
+    callback: (val: T) => RT,
+    or_val: RT
+) => {
+    if (optional === null || optional === undefined) {
+        return or_val;
+    }
+
+    return callback(optional as T);
+};
