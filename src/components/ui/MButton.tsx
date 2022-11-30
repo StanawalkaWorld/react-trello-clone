67f34851a4cf65bac82e5ee5db58732097c2720f
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-11-30 18:52:42 +0100

    Added a hover effect for the button.

--- src/components/ui/MButton.tsx
+++ src/components/ui/MButton.tsx
@@ -6,7 +6,7 @@ function MButton(props: {
 }) {
     return (
         <button
-            className="py-2 px-4 rounded-md bg-blue-gray-900"
+            className="py-2 px-4 rounded-md dark:bg-blue-gray-900 hover:dark:bg-blue-gray-800"
             onClick={props.onClick}
         >
             {props.children}
