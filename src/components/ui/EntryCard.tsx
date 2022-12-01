e6ba221493c08241becdb3139d5d419fea3d7443
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-12-01 19:30:03 +0100

    Added an option do delete individual entries.

--- src/components/ui/EntryCard.tsx
+++ src/components/ui/EntryCard.tsx
@@ -1,9 +1,18 @@
+import { useStoreDispatch } from "../../hooks/storeHook";
+import { entryRemove } from "../../stores/entry";
 import { Entry } from "../../types/entry";
 
 function EntryCard(props: { entry: Entry }) {
+    const dispatch = useStoreDispatch();
+
     return (
         <div className="p-4 rounded-md dark:bg-blue-gray-900 w-60">
-            <h4 className="text-xl">{props.entry.title}</h4>
+            <h4 className="text-xl float-left">{props.entry.title}</h4>
+            <i
+                className="bi-trash float-right"
+                onClick={() => dispatch(entryRemove(props.entry.id))}
+            ></i>
+            <div className="clear-both"></div>
             <p>{props.entry.description}</p>
         </div>
     );
