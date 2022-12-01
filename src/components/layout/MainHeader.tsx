a1f9c61a8fb570ce2f77ce9ef03c79c3b7b6a836
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-12-01 19:56:09 +0100

    Added a logo and favicon

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -1,7 +1,12 @@
+import Logo from "../../assets/img/small-logo.webp";
+
 function MainHeader() {
     return (
         <header className="p-4 bg-to-blue-500 dark:bg-blue-gray-800">
-            <h1 className="text-5xl font-bold">Trello</h1>
+            <div className="flex items-center gap-4">
+                <img src={Logo} alt="Company logo" width={64} height={64} />
+                <h1 className="text-5xl font-bold">Trello</h1>
+            </div>
         </header>
     );
 }
