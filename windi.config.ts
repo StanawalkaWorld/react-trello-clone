1136e94023dcb3f8e1d15cb1a264e2766f108eac
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-12-01 19:22:48 +0100

    Added standard colors.

--- windi.config.ts
+++ windi.config.ts
@@ -7,6 +7,23 @@ export default defineConfig({
             fontFamily: {
                 roboto: ["Roboto", "sans-serif"],
             },
+            colors: {
+                success: "#059669",
+                error: "#e11d48",
+                info: "#0284c7",
+                secondary: "#57534e",
+            },
         },
     },
+    safelist: [
+        "border-success",
+        "border-error",
+        "border-info",
+        "border-secondary",
+
+        "hover:bg-success",
+        "hover:bg-error",
+        "hover:bg-info",
+        "hover:bg-secondary",
+    ],
 });
