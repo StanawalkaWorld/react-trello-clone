e04c9df08b7ede1e06538bb8b6bb90cd326ec3c0
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-12-02 20:10:52 +0100

    Better styling and UI improvements.

--- src/components/ui/CardHolder.tsx
+++ src/components/ui/CardHolder.tsx
@@ -4,31 +4,42 @@ import { BehaviorColor } from "../../types/colors";
 import { Entry } from "../../types/entry";
 import CircleButton from "./CircleButton";
 import EntryCard from "./EntryCard";
-import MButton from "./MButton";
 
 function CardHolder(props: { category: string; entries: Entry[] }) {
     const dispatch = useStoreDispatch();
 
+    const remove = () => dispatch(categoryRemove(props.category));
+
     return (
-        <div className="p-2 rounded-md dark:bg-blue-gray-800 flex flex-col gap-2">
-            <div>
-                <h3 className="text-2xl float-left">{props.category}</h3>
-                <div className="float-right">
-                    <CircleButton
-                        variant={BehaviorColor.ERROR}
-                        onClick={() => dispatch(categoryRemove(props.category))}
-                        big
+        <div className="rounded-md dark:bg-blue-gray-800 flex justify-between w-72">
+            <div className="p-4 w-full">
+                <div className="mb-6">
+                    <h3
+                        className="text-2xl overflow-ellipsis"
+                        title={props.category}
                     >
-                        <span>
-                            <i className="bi-trash"></i>
-                        </span>
-                    </CircleButton>
+                        {props.category}
+                    </h3>
                 </div>
+                {props.entries.map((entry) => (
+                    <>
+                        <EntryCard entry={entry} key={entry.id} />
+                        <div className="mb-2"></div>
+                    </>
+                ))}
             </div>
-            <div className="clear-both"></div>
-            {props.entries.map((entry) => (
-                <EntryCard entry={entry} key={entry.id} />
-            ))}
+            <div className="bg-blue-gray-700 rounded-md p-2 flex flex-col gap-2">
+                <CircleButton variant={BehaviorColor.ERROR} onClick={remove}>
+                    <span>
+                        <i className="bi-dash-lg"></i>
+                    </span>
+                </CircleButton>
+                <CircleButton onClick={remove}>
+                    <span>
+                        <i className="bi-pencil-square"></i>
+                    </span>
+                </CircleButton>
+            </div>
         </div>
     );
 }
--- src/components/ui/EntryCard.tsx
+++ src/components/ui/EntryCard.tsx
@@ -1,26 +1,43 @@
-import { useStoreDispatch } from "../../hooks/storeHook";
+import { useStoreDispatch, useStoreSelector } from "../../hooks/storeHook";
 import { entryRemove } from "../../stores/entry";
 import { Entry } from "../../types/entry";
 import { BehaviorColor } from "../../types/colors";
 import CircleButton from "./CircleButton";
+import { tagsFromIds } from "../../stores/tags";
 
 function EntryCard(props: { entry: Entry }) {
     const dispatch = useStoreDispatch();
+    const tags = useStoreSelector(tagsFromIds(props.entry.tags ?? []));
+
+    const remove = () => dispatch(entryRemove(props.entry.id));
 
     return (
-        <div className="p-4 rounded-md dark:bg-blue-gray-900 w-60">
-            <h4 className="text-xl float-left">{props.entry.title}</h4>
-            <div className="float-right">
-                <CircleButton
-                    variant={BehaviorColor.ERROR}
-                    onClick={() => dispatch(entryRemove(props.entry.id))}
-                >
-                    <i className="bi-trash"></i>
+        <div className="p-4 rounded-md dark:bg-blue-gray-900 w-full flex gap-4">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <CircleButton variant={BehaviorColor.ERROR} onClick={remove}>
+                    <i className="bi-dash-lg"></i>
+                </CircleButton>
+                <CircleButton onClick={remove}>
+                    <i className="bi-pencil-square"></i>
                 </CircleButton>
             </div>
-
-            <div className="clear-both"></div>
-            <p>{props.entry.description}</p>
+            <div>
+                <div className="flex flex-wrap gap-4">
+                    {tags.map((tag) => (
+                        <div
+                            className="rounded-md p-1 text-xs"
+                            style={{ backgroundColor: tag.color }}
+                        >
+                            {tag.name}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex">
+                    <div>
+                        <h4 className="text-xl">{props.entry.title}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
