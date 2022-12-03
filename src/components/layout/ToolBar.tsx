82ba8bb901f4e9a0843653981ba7ffd31c760bb4
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-12-03 17:44:50 +0100

    Created a basic card creation dialog.

--- src/components/layout/ToolBar.tsx
+++ src/components/layout/ToolBar.tsx
@@ -9,6 +9,10 @@ function ToolBar() {
 
     const createDialog = useRef<HTMLDialogElement>(null);
 
+    // New Card form refs
+    const titleRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
     const test_entry = () => {
         dispatch(
             addEntry({
@@ -23,31 +27,52 @@ function ToolBar() {
     const createEntry = () => {};
 
     return (
-        <div className="p-4 bg-blue-gray-700 children:mr-4">
-            <MButton onClick={test_entry} variant={BehaviorColor.SUCCESS}>
-                <>
-                    <i className="bi-plus-lg mr-2 text-lg"></i>
-                    Add a card
-                </>
-            </MButton>
-            <MButton
-                onClick={() => dispatch(entryClear())}
-                variant={BehaviorColor.ERROR}
+        <>
+            <div className="p-4 bg-blue-gray-700 children:mr-4">
+                <MButton
+                    onClick={() => createDialog.current?.showModal()}
+                    variant={BehaviorColor.SUCCESS}
+                >
+                    <>
+                        <i className="bi-plus-lg mr-2 text-lg"></i>
+                        Add a card
+                    </>
+                </MButton>
+                <MButton
+                    onClick={() => dispatch(entryClear())}
+                    variant={BehaviorColor.ERROR}
+                >
+                    <>
+                        <i className="bi-trash mr-2 text-lg"></i>
+                        Remove all cards
+                    </>
+                </MButton>
+            </div>
+            <dialog
+                ref={createDialog}
+                className="bg-blue-gray-800 text-white w-1/2"
             >
-                <>
-                    <i className="bi-trash mr-2 text-lg"></i>
-                    Remove all cards
-                </>
-            </MButton>
-            <dialog ref={createDialog}>
-                <form method="dialog" onSubmit={createEntry}>
+                <form
+                    method="dialog"
+                    onSubmit={createEntry}
+                    className="flex flex-col gap-4"
+                >
+                    <h4 className="text-4xl font-bold">Add a new card.</h4>
                     {/* Title textbox */}
+                    <input className="input" type="text" ref={titleRef} />
                     {/* Description textarea */}
+                    <textarea className="input" ref={descriptionRef}></textarea>
                     {/* Category datalist (https://www.tutorialrepublic.com/html-reference/html5-datalist-tag.php) */}
                     {/* Some kind of tags multi-select and creation */}
+
+                    <div className="flex justify-center">
+                        <MButton variant={BehaviorColor.SUCCESS}>
+                            <>Create!</>
+                        </MButton>
+                    </div>
                 </form>
             </dialog>
-        </div>
+        </>
     );
 }
 
